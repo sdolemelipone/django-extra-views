e49334786aaa501ffe086a9f4d7e2bcb050d8823
melipone <mark.gensler@gmail.com>
2019-06-20 03:49:28 +0100

    Added test_settings.py to assist with running tests.

---
+++ extra_views_tests/test_settings.py
@@ -0,0 +1,28 @@
+
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'APP_DIRS': True,
+}]
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'extra_views',
+    'extra_views_tests',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'extra_views_tests.urls'
+
+SECRET_KEY = 'something not very secret'
